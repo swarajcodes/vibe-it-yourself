df32955908d93c1a73af9768890926e211c46159
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2025-08-22 20:08:32 +0530

    next project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <Button >Click me</Button>
+    </div>
+  )
+}
+
+export default Page
